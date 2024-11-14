c3c1bdf160a84b4ab3a175ab3a2e1538930f70fd
MWIMA123MABLE <mablecasandra@gmail.com>
2024-11-14 23:17:34 +0300

    python
    This rep contains python concept about variables

---
+++ practice/prac.py
@@ -0,0 +1,24 @@
+#prog that rints the users name and greats them 
+name = 'mable'
+print('hello',name)
+sum = 2+2
+print(sum)
+print(type(name))
+
+DOB =2004
+CURRENT_YEAR =2024
+AGE =CURRENT_YEAR-DOB
+print('MY AGE IS:',AGE)
+print(type(DOB))
+
+VAL_1 = int(input('Enter a number:'))
+VAL_2 = int(input ('Enter a number:'))
+sum = VAL_1+ VAL_2
+product = VAL_1 * VAL_2
+print('the sum is:',sum)
+print('the product is:',product)
+
+username = (input("enter a name:"))
+gender = (input("enter gender:"))
+print("the name is:",username)
+print("the gender is:",gender)
